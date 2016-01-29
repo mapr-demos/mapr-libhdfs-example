63d2fb57f4421c98bfc8a6089772829637b21e71
Vince Gonzalez <vince@ip-192-168-100-120.ec2.internal>
2016-01-29 12:35:27 -0500

    remove JVM dependency

--- compile.sh
+++ compile.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 export JAVA_HOME=/etc/alternatives/java_sdk
-export CFLAGS="-O2 -g -Wall -I$JAVA_HOME/include/ -I$JAVA_HOME/include/linux/ -I/opt/mapr/hadoop/hadoop-0.20.2/src/c++/libhdfs"
-export LDFLAGS="-lhdfs -L$JAVA_HOME/jre/lib/amd64/server/ -L/opt/mapr/hadoop/hadoop-0.20.2/c++/lib -ljvm"
-export LD_LIBRARY_PATH="$JAVA_HOME/jre/lib/amd64/server/ -L/opt/mapr/hadoop/hadoop-0.20.2/c++/lib -ljvm"
+export CFLAGS="-O2 -g -Wl,--allow-shlib-undefined -I$JAVA_HOME/include/ -I$JAVA_HOME/include/linux/ -I/opt/mapr/hadoop/hadoop-0.20.2/src/c++/libhdfs"
+export LDFLAGS=" -L$JAVA_HOME/jre/lib/amd64/server/ -L/opt/mapr/hadoop/hadoop-0.20.2/c++/lib -L/opt/mapr/lib -lMapRClient"
+export LD_LIBRARY_PATH="$JAVA_HOME/jre/lib/amd64/server/ -L/opt/mapr/hadoop/hadoop-0.20.2/c++/lib -lMapRClient"
 
 gcc $CFLAGS $LDFLAGS test.c -o test
 gcc $CFLAGS $LDFLAGS testRead.c -o testRead
